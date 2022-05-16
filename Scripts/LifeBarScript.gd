5840af25253047de32227627215a4d4a1138a9d3
MiguelAnchico <mangel.anchico@gmail.com>
2022-05-16 02:43:47 -0500

    Puerta anadida

--- Scripts/LifeBarScript.gd
+++ Scripts/LifeBarScript.gd
@@ -1,5 +1,13 @@
 extends ProgressBar
 
-	
-	
+var progreso = 0
 
+
+func _process(delta):
+	get_node(".").value = progreso
+	
+	if progreso == 25:
+		retirarPrimeraPuerta()
+
+func retirarPrimeraPuerta():
+	get_node("../../../../Puertas/primeraPuerta").queue_free()
